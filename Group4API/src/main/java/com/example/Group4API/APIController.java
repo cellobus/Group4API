8ffe8dfb7db425930b5410442c449012e0a7fc3e
HM\FRBUL <fredrik.bullsimonsen@hm.com>
2020-12-04 13:24:41 +0100

    no message

--- Group4API/src/main/java/com/example/Group4API/APIController.java
+++ Group4API/src/main/java/com/example/Group4API/APIController.java
@@ -1,5 +1,6 @@
 package com.example.Group4API;
 
+        import org.springframework.http.HttpStatus;
         import org.springframework.web.bind.annotation.*;
         import org.springframework.web.client.RestTemplate;
 
@@ -16,9 +17,9 @@ public class APIController {
     }
 
     @GetMapping("/shop/{id}") // HTTP status - Return code 404 Not found...
-    public Product getproduct(@PathVariable int id)   {
-        for(Product p :  productRepository.products)   {
-            if (p.getId() == id)  {
+    public Product getproduct(@PathVariable int id) {
+        for (Product p : productRepository.products) {
+            if (p.getId() == id) {
                 return p;
             }
         }
@@ -26,47 +27,36 @@ public class APIController {
     }
 
     @DeleteMapping("/shop/{id}")   // HTTP status - Return code 404 Not found...
-    public String  deleteProduct(@PathVariable int id)   {
-        for(Product p :  productRepository.products)   {
-            if (p.getId() == id)  {
+    public String deleteProduct(@PathVariable int id) {
+        for (Product p : productRepository.products) {
+            if (p.getId() == id) {
                 productRepository.products.remove(p);
                 return "Deleted";
             }
         }
+        
         return "Not found";
     }
 
+
     @PostMapping("/shop") // HTTP status - create - Return code 201
-    public Product createProduct(@RequestBody Product product)   {
+    public Product createProduct(@RequestBody Product product) {
         product.setCreatedAt("");
         product.setDiscountInPercentage(0.0);
         productRepository.addProduct(product);
-        return  product;
+        return product;
     }
 
     @PutMapping("/shop/{id}") // HTTP status - Return code 200 - OK
-    public Product updateProduct(@PathVariable int id, @RequestBody Product product)   {
+    public Product updateProduct(@PathVariable int id, @RequestBody Product product) {
         Product aProduct = productRepository.updateProduct(product, id);// getProduct(product.getId());
         aProduct = product;
-       // productRepository.products.add(aProduct);
+        // productRepository.products.add(aProduct);
         return aProduct;//(Book)bookRepository.getBook(aBook);
     }
-
-
-/*
-
-    @PutMapping(value="/book/{bookId}")
-    public Book updateBook(@PathVariable long bookId, @RequestBody Book book)   {
-        Book aBook = bookRepository.getBook(book.getId());
-        aBook = book;
-        return aBook;//(Book)bookRepository.getBook(aBook);
-    }
-    @PostMapping("/book")
-    public Book create(@RequestBody Book book) {
-        return bookRepository.addBook(book);
-    }
-
- */
 }
+
+
+
 
 
--- Group4API/src/test/java/com/example/Group4API/Group4ApiApplicationTests.java
+++ Group4API/src/test/java/com/example/Group4API/Group4ApiApplicationTests.java
@@ -1,13 +1,61 @@
 package com.example.Group4API;
 
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class Group4ApiApplicationTests {
+@AutoConfigureMockMvc
+
+public class Group4ApiApplicationTests {
+
+	@Autowired
+	MockMvc mvc;
+
+	@Autowired
+	ObjectMapper mapper;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+	}
+
+	@Test
+	public void testGetProductList() throws Exception {
+		mvc.perform(
+				MockMvcRequestBuilders.get("/shop")
+		)
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(MockMvcResultMatchers.content().string(containsString("T-Shirt")));
+	}
+	@Test
+	public void testPosProduct() throws Exception {
+		mvc.perform(
+				MockMvcRequestBuilders.post("/shop")
+						.content(mapper.writeValueAsString(new Product(100, "Jacka", 1, "Outdoor", "Winter jacket", 440, 0, "", "")))
+						.contentType(MediaType.APPLICATION_JSON_UTF8)
+		)
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(MockMvcResultMatchers.content().string(containsString("Jacka")));
+
+
+		mvc.perform(
+				MockMvcRequestBuilders.get("/book")
+		)
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(MockMvcResultMatchers.content().string(containsString("New Book")));
 	}
 
 }
